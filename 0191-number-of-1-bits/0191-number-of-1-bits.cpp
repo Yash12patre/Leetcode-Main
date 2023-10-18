4ad828fbd62fc0fd605983ceacf76cf1eb8da2c4
Yash Barapatre <92026630+Yash12patre@users.noreply.github.com>
2023-10-18 22:59:04 +0530

    Time: 4 ms (31.18%), Space: 6.5 MB (13.08%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
         while(n != 0){
-            int rmsb = n & -n;
-            n -= rmsb;
+            int nas = n & -n;
+            n -= nas;
             count++;
         }
         return count;
